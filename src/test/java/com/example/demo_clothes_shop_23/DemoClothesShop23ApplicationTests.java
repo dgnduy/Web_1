b8f76cf0ba8c80e1b9f420c628bde90ff7daa25e
seven-203 <duongngocduyk47@gmail.com>
2025-07-07 07:37:05 +0700

    Initial commit

---
+++ src/test/java/com/example/demo_clothes_shop_23/DemoClothesShop23ApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example.demo_clothes_shop_23;
+
+import com.example.demo_clothes_shop_23.entities.*;
+import com.example.demo_clothes_shop_23.model.enums.*;
+import com.example.demo_clothes_shop_23.repository.*;
+import com.example.demo_clothes_shop_23.service.ProductService;
+import com.github.javafaker.Faker;
+import com.github.slugify.Slugify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.logging.Logger;
+
+
+@SpringBootTest
+class DemoClothesShop23ApplicationTests {
+}
